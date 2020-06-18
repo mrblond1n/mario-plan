00552cc3405efcc8e5446067accbb8fa18066e8a
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-06-18 13:08:53 +0300

    add dashboard on home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import { HOME } from './components/constants/routers';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path={HOME} component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function ProjectList() {
   return (
-    <div>
-
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <div className="card-title">Project Title</div>
+          <p>Post by the Nikita</p>
+          <p className="grey-text">3rd September, 2am</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <div className="card-title">Project Title</div>
+          <p>Post by the Nikita</p>
+          <p className="grey-text">3rd September, 2am</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <div className="card-title">Project Title</div>
+          <p>Post by the Nikita</p>
+          <p className="grey-text">3rd September, 2am</p>
+        </div>
+      </div>
     </div>
   )
 }

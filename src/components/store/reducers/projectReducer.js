7a8ffc390d8f44667d87c7abd3e8f66c3149eceb
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-06-18 14:10:21 +0300

    add redux adn include this

---
+++ src/components/store/reducers/projectReducer.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE = {}
+
+const projectReducer = (state = INITIAL_STATE, action) => {
+  return state
+}
+
+export default projectReducer
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './components/store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
+let app = <Provider store={store}><App /></Provider>;
+let elemApp = document.getElementById('root')
+
+
+render(app, elemApp);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

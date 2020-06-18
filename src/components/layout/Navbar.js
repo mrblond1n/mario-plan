3f5a5b21d42bb2b3080653b7211904f09981f9cb
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-06-18 12:49:11 +0300

    include materailcss

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HOME } from '../constants/routers';
 
 export default function Navbar() {
   return (
-    <div>
-
-    </div>
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to={HOME} className="brand-logo">MarioPlan</Link>
+      </div>
+    </nav>
   )
 }

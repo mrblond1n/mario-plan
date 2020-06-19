019487cbc8d57572825e47858bfdb5cfd5b2c0cd
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-06-19 13:36:58 +0300

    set settings for get project from firebase

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,20 +1,35 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
-export default function ProjectDetail(props) {
+const ProjectDetail = props => {
   const id = props.match.params.id;
-
-  return (
-    <div className="container section project-details">
+  const projects = useSelector(state => state.firestore.data.projects)
+  const project = projects ? projects[id] : null
+  if (project) {
+    return (<div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <div className="card-title">Project Title - {id}</div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus quas voluptatibus hic ex autem perspiciatis ipsum praesentium cumque inventore.</p>
+          <div className="card-title">{project.title}</div>
+          <p>{project.content}</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by Nikita</div>
+          <div>{project.authorFirstName} {project.authorLastName}</div>
           <div>2rd september, 2am</div>
         </div>
       </div>
+    </div>)
+  }
+  return (
+    <div className="container center">
+      <p>Loading project...</p>
     </div>
   )
 }
+
+export default compose(
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(ProjectDetail)
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 export default function ProjectList({ projects }) {
+  console.log(projects);
 
   return (
     <div className="project-list section">
-      {projects && projects.map(project => <ProjectSummary project={project} key={project.id} />)}
+      {projects && projects.map(project => <Link to={`/project/${project.id}`} key={project.id}><ProjectSummary project={project} /></Link>)}
     </div>
   )
 }
